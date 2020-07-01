b21ea794649f3f48373c7406990f1419f13643a4
sakshamthukral <thukral.saksham132@gmail.com>
2020-07-01 13:12:30 +0530

    Updated Caption_it.py

--- Caption_it.py
+++ Caption_it.py
@@ -21,13 +21,11 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 from keras.layers import Input,Dense,Dropout,Embedding,LSTM
 from keras.layers.merge import add
+import warnings
+warnings.filterwarnings("ignore")
 
-
+print("Main Model Loaded")
 # In[20]:
-
-global graph
-graph = tf.get_default_graph()
-
 
 model = load_model("model_weights/model_29.h5")
 model._make_predict_function() # to prevent Value error tensor tensor in tensor flow, this function tell the model to use the current backend ie tensorflow to load the model
@@ -45,10 +43,10 @@ model_temp = ResNet50(weights="imagenet",input_shape=(224,224,3))
 model_resnet = Model(model_temp.input,model_temp.layers[-2].output)
 model_resnet._make_predict_function()  # to prevent Value error tensor tensor in tensor flow, this function tell the model to use the current backend ie tensorflow to load the model
 
-
+print("Resnet Model Loaded successfully")
 # In[23]:
 
-
+print("Session Created")
 def preprocess_img(img):
     img = image.load_img(img,target_size=(224,224))
     img = image.img_to_array(img)
@@ -63,16 +61,10 @@ def preprocess_img(img):
 
 def encode_image(img):
     img = preprocess_img(img)
-    with graph.as_default():
-        session = keras.backend.get_session()
-        init = tf.global_variables_initializer()
-        session.run(init)
-        keras.backend.set_session(session)
-        feature_vector = model_resnet.predict(img)
-    
-        feature_vector = feature_vector.reshape(1,feature_vector.shape[1]) # we want the shape to be (1,2048)
-        #print(feature_vector.shape)
-        return feature_vector
+    feature_vector = model_resnet.predict(img)
+    feature_vector = feature_vector.reshape(1,feature_vector.shape[1]) # we want the shape to be (1,2048)
+    #print(feature_vector.shape)
+    return feature_vector
 
 # In[29]:
 # In[30]:
@@ -89,40 +81,29 @@ with open("./storage/idx_to_word.pkl",'rb') as i2w:
 def predict_caption(photo):
     in_text = "startseq"
     max_len = 38
-    with graph.as_default():
-        session = keras.backend.get_session()
-        init = tf.global_variables_initializer()
-        session.run(init)
-        keras.backend.set_session(session)
-        for i in range(max_len):
-            sequence = [word_to_idx[w] for w in in_text.split() if w in word_to_idx]
-            sequence = pad_sequences([sequence],maxlen = max_len,padding='post')
+    for i in range(max_len):
+        sequence = [word_to_idx[w] for w in in_text.split() if w in word_to_idx]
+        sequence = pad_sequences([sequence],maxlen = max_len,padding='post')
         
-            ypred = model.predict([photo,sequence])
-            ypred = ypred.argmax() # take the word with maximum probability always-this type of sampling is also known as Greedy Sampling
-            word = idx_to_word[ypred]
-            in_text += (' ' + word)
+        ypred = model.predict([photo,sequence])
+        ypred = ypred.argmax() # take the word with maximum probability always-this type of sampling is also known as Greedy Sampling
+        word = idx_to_word[ypred]
+        in_text += (' ' + word)
         
-            if word == 'endseq':
-                break
-        # Final caption will be the in_text after removing 'startseq' and 'endseq'
-        final_caption = in_text.split()[1:-1] # removing staring word that is startseq and the ending word that os endseq
-        final_caption = ' '.join(final_caption)
+        if word == 'endseq':
+            break
+    # Final caption will be the in_text after removing 'startseq' and 'endseq'
+    final_caption = in_text.split()[1:-1] # removing staring word that is startseq and the ending word that os endseq
+    final_caption = ' '.join(final_caption)
     
-        return final_caption
+    return final_caption
 
 
 # In[32]:
 def caption_this_image(image):
     enc = encode_image(image)
-    with graph.as_default():
-        session = keras.backend.get_session()
-        init = tf.global_variables_initializer()
-        session.run(init)
-        keras.backend.set_session(session)
-
-        caption = predict_caption(enc)
-        return caption
+    caption = predict_caption(enc)
+    return caption
 
 
 
